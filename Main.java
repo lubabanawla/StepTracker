8ff3cfa2d2e0edf2b697a5235544b83dc1d60537
Lubaba Nawla <lubabanawla@gmail.com>
2025-05-04 19:25:44 -0400

    Add files via upload
    Finished

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main{
+    public static void main(String[] args){
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        StepTracker tr = new StepTracker(10000);
+
+        System.out.println(tr.activeDays());
+        System.out.println(tr.averageSteps());
+
+        tr.addDailySteps(9000);
+        tr.addDailySteps(5000);
+
+        System.out.println(tr.activeDays());
+        System.out.println(tr.averageSteps());
+
+        tr.addDailySteps(13000);
+
+        System.out.println(tr.activeDays());
+        System.out.println(tr.averageSteps());
+
+        tr.addDailySteps(23000);
+        tr.addDailySteps(1111);
+
+        System.out.println(tr.activeDays());
+        System.out.println(tr.averageSteps());
+
     }
 }
--- StepTracker.java
+++ StepTracker.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class StepTracker {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class StepTracker{
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    private int days;
+    private int steps;
+    private int threshold;
+    private int activeDays;
+
+    public StepTracker(int t) {
+        threshold = t;
     }
+
+    public int activeDays() {
+        return activeDays;
+    }
+
+    public double averageSteps() {
+        if(days == 0) return 0;
+        return (double) steps/days;
+    }
+
+    public void addDailySteps(int s) {
+        days++;
+        steps += s;
+        if(s>=threshold) activeDays++;
+    }
+
 }
